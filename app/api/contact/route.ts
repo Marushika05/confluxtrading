34f4f5ba3a872ff76a37dbf9020565cea57cbf30
Marushika05 <marushikagupta@gmail.com>
2026-05-28 17:33:38 +0530

    Configured Supabase on Vercel

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,12 +1,36 @@
 import { NextResponse } from "next/server";
 
+import { supabase } from "@/lib/supabase";
+
 export async function POST(req: Request) {
 
   const body = await req.json();
 
-  console.log(body);
+  const { name, phone, email, message } = body;
+
+  const { data, error } = await supabase
+    .from("leads")
+    .insert([
+      {
+        name,
+        phone,
+        email,
+        message,
+      },
+    ]);
+
+  if (error) {
+
+    console.log(error);
+
+    return NextResponse.json({
+      success: false,
+    });
+
+  }
 
   return NextResponse.json({
     success: true,
+    data,
   });
 }
