894f14152b52e119b7d49466152cec28ccaf9c73
Marushika05 <marushikagupta@gmail.com>
2026-07-21 17:09:34 +0530

    Fix commercial warranties page export

---
+++ app/commercial-warranties/page.tsx
@@ -0,0 +1,10 @@
+export default function CommercialWarrantiesPage() {
+  return (
+    <div>
+      <h1>Commercial Warranties</h1>
+      <p>
+        At ConfluxTrading, we stand behind the quality and reliability of our products.
+      </p>
+    </div>
+  );
+}
